f84536477efa932cf8ab1abc020fabe71bfb3acd
sanzio carmo <sanzio.carmo@gmail.com>
2025-02-04 19:33:21 -0300

    [04-sgs-config-server]

---
+++ sgs-config-server/04-sgs-config-server/src/main/java/br/com/sgs/SgsConfigServerApplication.java
@@ -0,0 +1,13 @@
+package br.com.sgs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgsConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgsConfigServerApplication.class, args);
+	}
+
+}
