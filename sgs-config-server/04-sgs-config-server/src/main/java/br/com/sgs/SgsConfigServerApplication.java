e401730727beb6ea5dab7b0bedd63b5c763404d2
sanzio carmo <sanzio.carmo@gmail.com>
2025-02-04 20:16:15 -0300

    [04-sgs-config-server] SGS - Microservices

--- sgs-config-server/04-sgs-config-server/src/main/java/br/com/sgs/SgsConfigServerApplication.java
+++ sgs-config-server/04-sgs-config-server/src/main/java/br/com/sgs/SgsConfigServerApplication.java
@@ -2,8 +2,10 @@ package br.com.sgs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class SgsConfigServerApplication {
 
 	public static void main(String[] args) {
