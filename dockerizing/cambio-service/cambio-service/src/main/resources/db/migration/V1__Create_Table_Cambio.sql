a8e828bd7600616016534f5be4e6341c1fe25c3f
sanzio carmo <sanzio.carmo@gmail.com>
2025-03-17 16:38:09 -0300

    [Dockerizing] SGS - Microservices

--- dockerizing/cambio-service/cambio-service/src/main/resources/db/migration/V1__Create_Table_Cambio.sql
+++ dockerizing/cambio-service/cambio-service/src/main/resources/db/migration/V1__Create_Table_Cambio.sql
@@ -1,5 +1,5 @@
 ﻿CREATE TABLE `cambio` (
-  `id` INT(10) AUTO_INCREMENT PRIMARY KEY,
+  `id` INT AUTO_INCREMENT PRIMARY KEY,
   `from_currency` CHAR(3) NOT NULL,
   `to_currency` CHAR(3) NOT NULL,
   `conversion_factor` decimal(65,2) NOT NULL
